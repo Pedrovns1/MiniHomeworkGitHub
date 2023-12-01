7b79dc654e5343de657c2f3a57d77d0021104d08
Pedro Vinicius <peuvini.94@gmail.com>
2023-12-01 17:09:33 +0000

    Attributes

---
+++ src/minihomeworkgithub/Data.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minihomeworkgithub;
+
+/**
+ *
+ * @author peuvi
+ */
+public class Data {
+    String Model;
+    String Make;
+    String Color;
+    int NumWheels;
+
+    public Data(String Model, String Make, String Color, int NumWheels) {
+        this.Model = Model;
+        this.Make = Make;
+        this.Color = Color;
+        this.NumWheels = NumWheels;
+    }
+
+    
+    
+    
+}
